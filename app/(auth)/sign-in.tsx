846dc701dbb5755a49420fd8796de68019464f4d
Nivall <nivahmichael@gmail.com>
2025-10-27 21:46:56 +0300

    configured appwrite.config file

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,6 @@
 import CustomButton from '@/components/CustomButton';
 import CustomInput from '@/components/CustomInput';
+import { signIn } from '@/lib/appwrite';
 import { Link, router } from 'expo-router';
 import React, { useState } from 'react';
 import { Alert, Text, View } from 'react-native';
@@ -9,11 +10,12 @@ const SignIn = () => {
   const [form, setForm] = useState({email:'', password:''});
 
   const submit = async() => {
-    if(!form.email || !form.password) return Alert.alert('Error', 'Please enter valid email address & passsword');
+    const {email, password} = form;
+    if(!email || !password) return Alert.alert('Error', 'Please enter valid email address & passsword');
       setIsSubmitting(true);
 
       try {
-        // call Appwrite Sign In ()=>
+        await signIn({email, password})
         
         Alert.alert('Success', 'User signed in successfully');
         router.replace('/')
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,5 +1,6 @@
 import CustomButton from '@/components/CustomButton';
 import CustomInput from '@/components/CustomInput';
+import { createUser } from '@/lib/appwrite';
 import { Link, router } from 'expo-router';
 import React, { useState } from 'react';
 import { Alert, Text, View } from 'react-native';
@@ -9,13 +10,14 @@ const SignUp = () => {
   const [form, setForm] = useState({name: '', email: '', password: ''});
 
   const submit = async() => {
-    if(!form.name ||!form.email || !form.password) return Alert.alert('Error', 'Please enter valid email address & passsword');
+    const {name, email, password} = form;
+    if(!name ||!email || !password) return Alert.alert('Error', 'Please enter valid email address & passsword');
       setIsSubmitting(true);
 
       try {
-        // call Appwrite Sign Up ()=>
+        await createUser({ email, password, name })
         
-        Alert.alert('Success', 'User signed up successfully');
+        //Alert.alert('Success', 'User signed up successfully');
         router.replace('/')
       } catch(error: any) {
         Alert.alert('Error', error.message)
