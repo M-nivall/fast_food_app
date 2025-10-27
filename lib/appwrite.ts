051beac2675b166d9324b1227276d16c5ff5d2d1
Nivall <nivahmichael@gmail.com>
2025-10-27 14:40:48 +0300

    Created the log in & sign in

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.mn.foodordering",
+    databaseId: '68f14fc80021974ee32f',
+    userColletionId: '68f15847003330a6a1a6',
+}
