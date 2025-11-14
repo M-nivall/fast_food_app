9c4f7aa6a52f692126b896f8bdd62400181f2400
Nivall <nivahmichael@gmail.com>
2025-11-15 00:30:27 +0300

    implemented the useFetch hook

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,21 +1,20 @@
 import { getCategories, getMenu } from '@/lib/appwrite'
 import useAppwrite from '@/lib/useAppwrite'
 import { useLocalSearchParams } from 'expo-router'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const search = () => {
   const {category, query} = useLocalSearchParams<{query: string; category: string}>()
-  const {data, refetch, loading} = useAppwrite({
-    fn: getMenu,
-    params: {
-      category,
-      query,
-      limit: 6
-    }
-  });
+
+  const {data, refetch, loading} = useAppwrite({ fn: getMenu, params: { category, query, limit: 6  } });
+
   const {data: categories} = useAppwrite({fn: getCategories})
+
+  useEffect(() => {
+    refetch({category, query, limit: 6});
+  },[category,query]);
 
   return (
     <SafeAreaView>
