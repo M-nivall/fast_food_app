d3d6218ce33ef2072ebed2484c2d7f3955b1e92f
Nivall <nivahmichael@gmail.com>
2025-11-16 01:01:09 +0300

    implemented the search input field

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,3 +1,4 @@
+import CartButton from '@/components/CartButton'
 import { getCategories, getMenu } from '@/lib/appwrite'
 import useAppwrite from '@/lib/useAppwrite'
 import { useLocalSearchParams } from 'expo-router'
@@ -27,6 +28,25 @@ const search = () => {
             </View>
           )
         }}
+        keyExtractor={item => item.$id}
+        numColumns={2}
+        columnWrapperClassName="gap-7"
+        contentContainerClassName="gap-7 px-5 pb-32"
+        ListHeaderComponent={() => (
+          <View className="my-5 gap-5">
+            <View className="flex-between flex-row w-full">
+              <View className="flex-start">
+                <Text className="small-bold uppercase text-primary">Search</Text>
+                <View className="flex-start flex-row gap-x-1 mt-0.5">
+                  <Text className="paragraph-semibold text-dark-100">Find your favorite food</Text>
+                </View>
+              </View>
+              <CartButton/>
+            </View>
+            <Text>Search input</Text>
+            <Text>Filter</Text>
+          </View>
+        )}
       />
     </SafeAreaView>
   )
