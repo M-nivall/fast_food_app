2b6f2cdcb408e4020dada4f2df15f83a4c23abb4
Nivall <nivahmichael@gmail.com>
2025-11-17 21:47:09 +0300

    implemented search text input handler

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { TextInput, View } from 'react-native'
+import { useLocalSearchParams } from 'expo-router';
+import React, { useState } from 'react';
+import { TextInput, View } from 'react-native';
 
 const SearchBar = () => {
+  const params = useLocalSearchParams<{ query?: string}>();
+  const [query, setQuery] = useState(params.query);
+
+  const handleSearch = (tex: string) => {}
   return (
     <View className="searchbar">
       <TextInput
         className="flex-1 p-5"
         placeholder="Search for pizzas, burgers..."
+        value={query}
+        onChangeText={handleSearch}
+        placeholderTextColor="#A0A0A0"
        />
     </View>
   )
