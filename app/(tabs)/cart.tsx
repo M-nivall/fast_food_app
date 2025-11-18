3fa762c936f9340642f7b61a8348f509be311e67
Nivall <nivahmichael@gmail.com>
2025-11-18 17:39:09 +0300

    implemented payment stripe

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,8 +1,21 @@
 import CustomHeader from '@/components/CustomHeader'
 import { useCartStore } from '@/store/cart.store'
+import { PaymentInfoStripeProps } from '@/type'
+import cn from 'clsx'
 import React from 'react'
 import { FlatList, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+
+const PaymentInfoStripe = ({ label,  value,  labelStyle,  valueStyle, }: PaymentInfoStripeProps) => (
+    <View className="flex-between flex-row my-1">
+        <Text className={cn("paragraph-medium text-gray-200", labelStyle)}>
+            {label}
+        </Text>
+        <Text className={cn("paragraph-bold text-dark-100", valueStyle)}>
+            {value}
+        </Text>
+    </View>
+);
 
 const cart = () => {
   const {items, getTotalItems, getTotalPrice} = useCartStore();
