94363a634085f2b5c7a791ff7d63da26ac0966c4
Nivall <nivahmichael@gmail.com>
2025-11-18 17:10:23 +0300

    implemented cart component

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,11 +1,34 @@
+import CustomHeader from '@/components/CustomHeader'
+import { useCartStore } from '@/store/cart.store'
 import React from 'react'
-import { Text } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const cart = () => {
+  const {items, getTotalItems, getTotalPrice} = useCartStore();
+
+  const totalItems = getTotalItems();
+  const totalPrice = getTotalPrice();
+
   return (
     <SafeAreaView className="bg-white h-full">
-      <Text>cart</Text>
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>Cart Item</Text>}
+        keyExtractor={(item) => item.id}
+        contentContainerClassName="pb-28 px-5 pt-5"
+        ListHeaderComponent={() => <CustomHeader title="Your Cart"/>}
+        ListEmptyComponent={() => <Text>Cart Empty</Text>}
+        ListFooterComponent={() => totalItems > 0 && (
+          <View className="gap-5">
+            <View className="mt-6 border border-gray-200 p-5 rounded-2xl">
+              <Text className="h3-bold text-dark-100 mb-5">
+                Payment Summary 
+              </Text>
+            </View>
+          </View>
+        )}
+       />
     </SafeAreaView>
   )
 }
