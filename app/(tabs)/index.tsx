c076a047e294c250567201d0e4612d494636529c
nival <nivahmichael@gmail.com>
2026-01-21 05:25:17 +0300

    implemented index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,20 @@
 import CartButton from "@/components/CartButton";
 import { images, offers } from "@/constants";
-import cn from 'clsx';
+import cn from "clsx";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../globals.css";
 
 export default function Index() {
   return (
-    
     <SafeAreaView className="flex-1 bg-white">
       <FlatList
         data={offers}
@@ -16,19 +22,34 @@ export default function Index() {
           const isEven = index % 2 == 0;
           return (
             <View>
-              <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')} 
-                style = {{ backgroundColor: item.color}}
-                android_ripple={{color:"#ffffff22", foreground: true}}
-                >
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row",
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#ffffff22", foreground: true }}
+              >
                 {({ pressed }) => (
                   <Fragment>
                     <View className="h-full w-1/2">
-                      <Image source={item.image} className="size-full" resizeMode="contain"/>
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
                     </View>
-                    <View className={cn("offer-card__info", isEven ? 'pl-10' : 'par-10')}>
-                      <Text className="h1-bold text-white leading-tight">{item.title}</Text>
-                      <Image 
-                        source={images.arrowRight} 
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "par-10",
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
                         className="size-10"
                         resizeMode="contain"
                         tintColor="#ffffff"
@@ -38,23 +59,26 @@ export default function Index() {
                 )}
               </Pressable>
             </View>
-          )
+          );
         }}
         contentContainerClassName="pb-28 px-5"
         ListHeaderComponent={() => (
-        <View className="flex-between flex-row w-full my-5">
-          <View className="flex-start"> 
+          <View className="flex-between flex-row w-full my-5">
+            <View className="flex-start">
               <Text className="small-bold text-primary">DELIVER WITHIN</Text>
               <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
                 <Text className="paragraph-bold text-dark-100">NAIROBI</Text>
-                <Image source={images.arrowDown} className="size-3 pt-5" resizeMode="contain"/>
-            </TouchableOpacity>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3 pt-5"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <CartButton />
           </View>
-          <CartButton/>
-        </View>
         )}
       />
     </SafeAreaView>
   );
-  
 }
