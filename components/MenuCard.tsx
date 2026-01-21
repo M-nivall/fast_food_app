5a25172649827dace118bb5364c280d6b69d6d5b
nival <nivahmichael@gmail.com>
2026-01-21 05:21:07 +0300

    implemented menu card

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,24 +1,26 @@
-import { appwriteConfig } from '@/lib/appwrite'
-import { useCartStore } from '@/store/cart.store'
-import { MenuItem } from '@/type'
-import React from 'react'
-import { Image, Platform, Text, TouchableOpacity } from 'react-native'
+import { appwriteConfig } from "@/lib/appwrite";
+import { useCartStore } from "@/store/cart.store";
+import { MenuItem } from "@/type";
+import React from "react";
+import { Image, Platform, Text, TouchableOpacity } from "react-native";
 
 interface MenuCardProps {
-  item: MenuItem
+  item: MenuItem;
 }
 
 const MenuCard = ({ item: { $id, image_url, name, price } }: MenuCardProps) => {
-  
   const imageUrl = `${image_url}?project=${appwriteConfig.projectId}`;
 
-  const {addItem} = useCartStore();
+  const { addItem } = useCartStore();
 
   return (
-    
     <TouchableOpacity
       className="menu-card pt-4" // small top padding
-      style={Platform.OS === 'android' ? { elevation: 10, shadowColor: '#878787' } : {}}
+      style={
+        Platform.OS === "android"
+          ? { elevation: 10, shadowColor: "#878787" }
+          : {}
+      }
     >
       <Image
         source={{ uri: imageUrl }}
@@ -33,16 +35,23 @@ const MenuCard = ({ item: { $id, image_url, name, price } }: MenuCardProps) => {
         {name}
       </Text>
 
-      <Text className="body-regular text-gray-200 mb-4">
-        From ${price}
-      </Text>
+      <Text className="body-regular text-gray-200 mb-4">From ${price}</Text>
 
-      <TouchableOpacity onPress={() => addItem({id: $id, name, price, image_url: imageUrl, customizations: []})}>
-        <Text className="paragraph-bold text-primary">
-          Add to Cart
-        </Text>
+      <TouchableOpacity
+        onPress={() =>
+          addItem({
+            id: $id,
+            name,
+            price,
+            image_url: imageUrl,
+            customizations: [],
+          })
+        }
+      >
+        <Text className="paragraph-bold text-primary">Add to Cart</Text>
       </TouchableOpacity>
     </TouchableOpacity>
-  )
-}
-export default MenuCard
+  );
+};
+
+export default MenuCard;
