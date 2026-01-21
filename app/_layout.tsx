7d986f687953c75e9a8afd814de65ccbf2db6b2e
nival <nivahmichael@gmail.com>
2026-01-21 05:24:28 +0300

    implemented layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,12 @@
-import useAuthStore from '@/store/auth.store';
-import * as Sentry from '@sentry/react-native';
-import { useFonts } from 'expo-font';
+import useAuthStore from "@/store/auth.store";
+import * as Sentry from "@sentry/react-native";
+import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import "./globals.css";
 
 Sentry.init({
-  dsn: 'https://3b1b8a90cf2bc609366dc4d519382e98@o4510263083925505.ingest.us.sentry.io/4510263130193920',
+  dsn: "https://3b1b8a90cf2bc609366dc4d519382e98@o4510263083925505.ingest.us.sentry.io/4510263130193920",
 
   // Adds more context data to events (IP address, cookies, user, etc.)
   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
@@ -18,21 +18,23 @@ Sentry.init({
   // Configure Session Replay
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: __DEV__,
 });
 
 export default Sentry.wrap(function RootLayout() {
-
-  const {isLoading, fetchAuthenticatedUser} = useAuthStore();
+  const { isLoading, fetchAuthenticatedUser } = useAuthStore();
   const [fontsLoaded, error] = useFonts({
-    "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
-    "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
-    "QuickSand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
-    "QuickSand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
-    "QuickSand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+    "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
   });
 
   useEffect(() => {
